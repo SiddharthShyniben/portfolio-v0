b51239799d052b600e6786c25c1b9dbaadf339f4
Siddharth Shyniben <siddharth.muscat@gmail.com>
2024-07-21 16:06:08 +0530

    Initial commit

---
+++ js/button.js
@@ -0,0 +1,216 @@
+const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+const calcWinsize = () => ({
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
+
+const getMousePos = (e) => ({
+  x: e.clientX,
+  y: e.clientY,
+});
+
+const distance = (x1, y1, x2, y2) => {
+  var a = x1 - x2;
+  var b = y1 - y2;
+
+  return Math.hypot(a, b);
+};
+
+// Calculate the viewport size
+let winsize = calcWinsize();
+window.addEventListener("resize", () => (winsize = calcWinsize()));
+
+// Track the mouse position
+let mouse = { x: 0, y: 0 };
+window.addEventListener("mousemove", (ev) => (mouse = getMousePos(ev)));
+
+class Cursor {
+  constructor(el) {
+    this.DOM = { el };
+    this.DOM.el.style.opacity = 0;
+
+    this.bounds = this.DOM.el.getBoundingClientRect();
+
+    this.renderedStyles = {
+      tx: { previous: 0, current: 0, amt: 0.2 },
+      ty: { previous: 0, current: 0, amt: 0.2 },
+      scale: { previous: 1, current: 1, amt: 0.2 },
+      opacity: { previous: 1, current: 1, amt: 0.2 },
+    };
+
+    this.onMouseMoveEv = () => {
+      this.renderedStyles.tx.previous = this.renderedStyles.tx.current =
+        mouse.x - this.bounds.width / 2;
+      this.renderedStyles.ty.previous = this.renderedStyles.ty.previous =
+        mouse.y - this.bounds.height / 2;
+      gsap.to(this.DOM.el, {
+        duration: 0.9,
+        ease: "Power3.easeOut",
+        opacity: 1,
+      });
+      requestAnimationFrame(() => this.render());
+      window.removeEventListener("mousemove", this.onMouseMoveEv);
+    };
+    window.addEventListener("mousemove", this.onMouseMoveEv);
+  }
+  enter() {
+    this.renderedStyles["scale"].current = 4;
+    this.renderedStyles["opacity"].current = 0.2;
+  }
+  leave() {
+    this.renderedStyles["scale"].current = 1;
+    this.renderedStyles["opacity"].current = 1;
+  }
+  render() {
+    this.renderedStyles["tx"].current = mouse.x - this.bounds.width / 2;
+    this.renderedStyles["ty"].current = mouse.y - this.bounds.height / 2;
+
+    for (const key in this.renderedStyles) {
+      this.renderedStyles[key].previous = lerp(
+        this.renderedStyles[key].previous,
+        this.renderedStyles[key].current,
+        this.renderedStyles[key].amt,
+      );
+    }
+
+    this.DOM.el.style.transform = `translateX(${this.renderedStyles["tx"].previous}px) translateY(${this.renderedStyles["ty"].previous}px) scale(${this.renderedStyles["scale"].previous})`;
+    this.DOM.el.style.opacity = this.renderedStyles["opacity"].previous;
+
+    requestAnimationFrame(() => this.render());
+  }
+}
+
+class ButtonCtrl extends EventTarget {
+  constructor(el) {
+    super();
+
+    this.DOM = { el: el };
+    this.DOM.text = this.DOM.el.querySelector(".button__text");
+    this.DOM.textinner = this.DOM.el.querySelector(".button__text-inner");
+
+    this.renderedStyles = {
+      tx: { previous: 0, current: 0, amt: 0.1 },
+      ty: { previous: 0, current: 0, amt: 0.1 },
+    };
+
+    this.state = {
+      hover: false,
+    };
+
+    this.events = {
+      enter: new Event("enter"),
+      leave: new Event("leave"),
+    };
+
+    this.calculateSizePosition();
+    this.initEvents();
+    requestAnimationFrame(() => this.render());
+  }
+  calculateSizePosition() {
+    // size/position
+    this.rect = this.DOM.el.getBoundingClientRect();
+    // the movement will take place when the distance from the mouse to the center of the button is lower than this value
+    this.distanceToTrigger = this.rect.width * 1;
+  }
+  initEvents() {
+    this.onResize = () => this.calculateSizePosition();
+    window.addEventListener("resize", this.onResize);
+  }
+  render() {
+    // calculate the distance from the mouse to the center of the button
+    const distanceMouseButton = distance(
+      mouse.x + window.scrollX,
+      mouse.y + window.scrollY,
+      this.rect.left + this.rect.width / 2,
+      this.rect.top + this.rect.height / 2,
+    );
+    // new values for the translations
+    let x = 0;
+    let y = 0;
+
+    if (distanceMouseButton < this.distanceToTrigger) {
+      if (!this.state.hover) {
+        this.enter();
+      }
+      x =
+        (mouse.x + window.scrollX - (this.rect.left + this.rect.width / 2)) *
+        0.3;
+      y =
+        (mouse.y + window.scrollY - (this.rect.top + this.rect.height / 2)) *
+        0.3;
+    } else if (this.state.hover) {
+      this.leave();
+    }
+
+    this.renderedStyles["tx"].current = x;
+    this.renderedStyles["ty"].current = y;
+
+    for (const key in this.renderedStyles) {
+      this.renderedStyles[key].previous = lerp(
+        this.renderedStyles[key].previous,
+        this.renderedStyles[key].current,
+        this.renderedStyles[key].amt,
+      );
+    }
+
+    this.DOM.el.style.transform = `translate3d(${this.renderedStyles["tx"].previous}px, ${this.renderedStyles["ty"].previous}px, 0)`;
+    this.DOM.text.style.transform = `translate3d(${-this.renderedStyles["tx"].previous * 0.6}px, ${-this.renderedStyles["ty"].previous * 0.6}px, 0)`;
+
+    requestAnimationFrame(() => this.render());
+  }
+  enter() {
+    this.dispatchEvent(this.events.enter);
+
+    this.state.hover = true;
+    this.DOM.el.classList.add("button--hover");
+
+    document.body.classList.add("active");
+
+    gsap.killTweensOf(this.DOM.textinner);
+
+    gsap
+      .timeline()
+      .to(this.DOM.textinner, 0.15, {
+        ease: "Power2.easeIn",
+        opacity: 0,
+        y: "-20%",
+      })
+      .to(this.DOM.textinner, 0.2, {
+        ease: "Expo.easeOut",
+        opacity: 1,
+        startAt: { y: "100%" },
+        y: "0%",
+      });
+  }
+  leave() {
+    this.dispatchEvent(this.events.leave);
+
+    this.state.hover = false;
+    this.DOM.el.classList.remove("button--hover");
+
+    document.body.classList.remove("active");
+
+    gsap.killTweensOf(this.DOM.textinner);
+
+    gsap
+      .timeline()
+      .to(this.DOM.textinner, 0.15, {
+        ease: "Power2.easeIn",
+        opacity: 0,
+        y: "20%",
+      })
+      .to(this.DOM.textinner, 0.2, {
+        ease: "Expo.easeOut",
+        opacity: 1,
+        startAt: { y: "-100%" },
+        y: "0%",
+      });
+  }
+}
+
+const cursor = new Cursor(document.querySelector(".cursor"));
+const button = new ButtonCtrl(document.querySelector(".button"));
+
+button.addEventListener("enter", () => cursor.enter());
+button.addEventListener("leave", () => cursor.leave());
---
+++ js/morph.js
@@ -0,0 +1,86 @@
+const morphElements = [
+  document.getElementById("morph1"),
+  document.getElementById("morph2"),
+];
+
+const texts = [
+  "stunning software",
+  "efficient architecture",
+  "innovative prototypes",
+  "clever solutions",
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = -1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+morphElements[0].textContent = texts[textIndex % texts.length];
+morphElements[1].textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+    cooldown = cooldownTime;
+    fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+// A lot of the magic happens here, this is what applies the blur filter to the text.
+function setMorph(fraction) {
+  fraction = Math.cos(fraction * Math.PI) / -2 + 0.5;
+
+  morphElements[1].style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  morphElements[1].style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  morphElements[0].style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  morphElements[0].style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  morphElements[0].textContent = texts[textIndex % texts.length];
+  morphElements[1].textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  morphElements[1].style.filter = "";
+  morphElements[1].style.opacity = "100%";
+
+  morphElements[0].style.filter = "";
+  morphElements[0].style.opacity = "0%";
+}
+
+// Animation loop, which is called every frame.
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 1000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+    if (shouldIncrementIndex) {
+      textIndex++;
+    }
+
+    doMorph();
+  } else {
+    doCooldown();
+  }
+}
+
+// Start the animation.
+animate();
